734493f0c9d950ad1c4ca96e8ef3694abd7ecf7b
Stephen Mayeux <stephenmayeux@gmail.com>
2017-11-30 20:47:28 -0600

    vote on items

--- public/main.js
+++ public/main.js
@@ -24,13 +24,39 @@ $(document).ready(function() {
       buildItemElement(result);
     });
 
+  database.ref('items')
+    .on('child_changed', function(snapshot) {
+      var result = snapshot.val();
+      result.id = snapshot.key;
+      console.log('working?', result)
+      $('div.votes#'+result.id).text(result.votes);
+  });
+
+  function upVote() {
+    var itemID = $(this).attr('id');
+    firebase.database().ref('/items/' + itemID + '/votes')
+      .transaction(function(currentVotes) {
+        return currentVotes + 1;
+      });
+  }
+
+  function downVote() {
+    var itemID = $(this).attr('id');
+    firebase.database().ref('/items/' + itemID + '/votes')
+      .transaction(function(currentVotes) {
+        return currentVotes - 1;
+      });
+  }
+
   function buildItemElement(item) {
     var $template = $('#content-template').clone();
     var newItem = $template.prop('content');
 
     $(newItem).find('.content-title').text(item.title);
     $(newItem).find('.arrow').attr('id', item.id);
-    $(newItem).find('.votes').text(item.votes);
+    $(newItem).find('.vote-up').on('click', upVote);
+    $(newItem).find('.vote-down').on('click', downVote);
+    $(newItem).find('.votes').text(item.votes).attr('id', item.id);
     $(newItem).find('.content-link').attr('href', item.link).attr('target', '_blank');
     $(newItem).find('.content-meta').text(item.user + ' posted at ' + moment(item.createdAt).fromNow());
 
