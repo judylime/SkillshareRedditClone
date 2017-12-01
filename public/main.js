99d0721392ba9002d5d4a4465e2ae20e2906337b
Stephen Mayeux <stephenmayeux@gmail.com>
2017-11-30 19:27:52 -0600

    fetch data

--- public/main.js
+++ public/main.js
@@ -7,43 +7,26 @@ $(document).ready(function() {
     storageBucket: "skillshareredditclone.appspot.com",
     messagingSenderId: "868707919715"
   };
-  var createdAt = new Date();
-  var dummyData = [
-    {
-      title: 'Funny Video!',
-      link: 'https://www.youtube.com',
-      votes: 32,
-      user: 'Stephen',
-      createdAt: createdAt
-    },
-    {
-      title: 'Funny Video!',
-      link: 'https://www.youtube.com',
-      votes: 32,
-      user: 'Stephen',
-      createdAt: createdAt
-    },
-    {
-      title: 'Funny Video!',
-      link: 'https://www.youtube.com',
-      votes: 32,
-      user: 'Stephen',
-      createdAt: createdAt
-    }
-  ];
 
   firebase.initializeApp(config);
   var database = firebase.database();
 
-  dummyData.forEach(function(item) {
+  database.ref('/items').once('value').then(function(snapshot) {
+    var results = snapshot.val();
+    for (var id in results) {
+      buildItemElement(results[id]);
+    }
+  });
+
+  function buildItemElement(item) {
     var $template = $('#content-template').clone();
     var newItem = $template.prop('content');
 
     $(newItem).find('.content-title').text(item.title);
     $(newItem).find('.votes').text(item.votes);
     $(newItem).find('.content-link').attr('href', item.link).attr('target', '_blank');
-    $(newItem).find('.content-meta').text(item.user + ' posted at ' + item.createdAt);
+    $(newItem).find('.content-meta').text(item.user + ' posted at ' + moment(item.createdAt).fromNow());
 
     $('#list').append(newItem);
-  })
+  }
 });
